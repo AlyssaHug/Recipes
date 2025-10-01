f65a9fcb0d0e96bc349817ca0b9c45754054c989
AlyssaHug <AlyssaHug@shaw.ca>
2025-09-30 18:38:57 -0700

    implemented TheMealDB API + tested with old styles from book site

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+function Recipe({ recipe }) {
+    return (
+        <div>
+            <img
+                className='cardImg'
+                src={recipe.strMealThumb}
+                alt={recipe.strMeal}
+            />
+            <h3 className='recipe_name'>{recipe.strMeal}</h3>
+            <h3 className='view'>
+                <a
+                    href={recipe.strSource || recipe.strYoutube}
+                    target='_blank'>
+                    View Full Recipe
+                </a>
+            </h3>
+        </div>
+    );
+}
+
+export default Recipe;
