cfa7f17a57ce61fecd0dd4f5bd397e635d0babd4
Sandy Chow <sandychow@Sandys-MacBook-Pro.local>
2025-10-02 02:40:36 -0700

    Create NewForm and Modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,22 @@
+import {useRef} from "react";
+import NewForm from "../NewForm/NewForm";
+import "./Modal.css";
+import "../NewForm/NewForm.css";
+
+function Modal({btnLabel, btnClassName}) {
+    const modalRef = useRef();
+
+    function handleclick(e) {
+        modalRef.current.showModal();
+    }
+
+    return (
+        <>
+        <button className={btnClassName} onClick={handleclick}> {btnLabel} </button>
+        <dialog ref={modalRef} className="modal">
+            <NewForm />
+        </dialog>
+        </>
+    );
+}
+    export default Modal;
