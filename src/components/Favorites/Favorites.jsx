bd8ead1f1317784a42ba656bb2a1e48b1444fe32
Sandy Chow <sandychow@Sandys-MacBook-Pro.local>
2025-11-15 23:01:58 -0800

    edit favorite buttons content

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -3,7 +3,7 @@ import "./Favorites.css";
 function Favorites({ showFavorites, onToggle }) {
   return (
     <button className="fav-filter" onClick={onToggle}>
-      {showFavorites ? "Show All" : "Show Favorites"}
+      {showFavorites ? "Show All Recipes" : "Show Favorites"}
     </button>
   );
 }
